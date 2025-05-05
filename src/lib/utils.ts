ffede8434aaf4960313d38c4c1ddff665c5996a7
swapnilkeshari <swk25@pitt.edu>
2025-05-05 19:59:04 -0400

    v2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx } from "clsx";
+// import { clsx } from "clsx";
+// import { twMerge } from "tailwind-merge";
+
+// export function cn(...inputs) {
+//   return twMerge(clsx(inputs));
+// }
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
